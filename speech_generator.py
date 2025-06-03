fdc8aeda74cf128487a006f6d9c4de861659ab4c
yyw28 <59491265+yyw28@users.noreply.github.com>
2025-06-02 21:05:31 -0700

    Delete speech_generator.py

--- speech_generator.py
+++
@@ -1,8 +0,0 @@
-from ssml_reader import SsmlReader
-
-if __name__ == '__main__':
-    reader: SsmlReader = SsmlReader()
-    reader.extract_file_ssml("/Users/ghaz/documents/research_speech/ssmls/speech_test.txt")
-    
-    reader.save_file_contents_as_speech()
-    
